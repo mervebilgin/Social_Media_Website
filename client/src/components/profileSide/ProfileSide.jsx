affcf5dcb0a716d738a81546340d0f4f282f794e
bilginmrv <mrvblgn44@gmail.com>
2022-11-05 14:03:02 +0300

    Create home page

---
+++ client/src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ProfileSide.css"
+
+const ProfileSide = () => {
+  return (
+    <div className="ProfileSide">
+        ProfileSide Section
+    </div>
+  )
+}
+
+export default ProfileSide
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProfileSide from '../../components/profileSide/ProfileSide'
+
+import './Home.css'
+const Home = () => {
+  return (
+    <div className="Home">
+        <ProfileSide/>
+        <div className='postSide'>Posts</div>
+        <div className='RightSide'>RightSide</div>
+    </div>
+  )
+}
+
+export default Home
