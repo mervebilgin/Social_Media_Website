5394198e3eca64db84de44637a27b25441cafc42
bilginmrv <mrvblgn44@gmail.com>
2022-11-05 13:48:55 +0300

    Create App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-        Hello, I am Merve
+        <div className="blur" style={{top: '-18%', right: '0'}}></div>
+        <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
+        
     </div>
   );
 }
