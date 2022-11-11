0d1e65300cb6cb9762f504fb98d39830c2843065
bilginmrv <mrvblgn44@gmail.com>
2022-11-11 18:40:45 +0300

    Create auth controller

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { loginUser, registerUser } from "../Controllers/AuthController.js";
 
 const router = express.Router()
 
-router.get('/', async(req, res)=>{res.send("Auth Route")})
-
+router.post('/register', registerUser)
+router.post('/login', loginUser)
 export default router
