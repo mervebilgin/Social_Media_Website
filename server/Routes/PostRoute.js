4a89bff9c2f4e983438cf2dd292fef0083a78bfe
bilginmrv <mrvblgn44@gmail.com>
2022-11-11 22:12:20 +0300

    Update PostRoute.js

--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { createPost, deletePost, getPost, getTimelinePosts, likePost, updatePost } from "../Controllers/PostController.js";
-const router = express.Router()
+import {
+  createPost,
+  deletePost,
+  getPost,
+  getTimelinePosts,
+  likePost,
+  updatePost,
+} from "../Controllers/PostController.js";
+const router = express.Router();
 
-router.post('/', createPost)
-router.get('/:id', getPost)
-router.put('/:id', updatePost)
-router.delete("/:id", deletePost)
-router.put("/:id/like", likePost)
-router.get("/:id/timeline", getTimelinePosts)
+router.post("/", createPost);
+router.get("/:id", getPost);
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.put("/:id/like", likePost);
+router.get("/:id/timeline", getTimelinePosts);
 export default router;
