b29d78ff7d97779034a63367e7a505cfc7cb85c6
bilginmrv <mrvblgn44@gmail.com>
2022-11-11 21:27:45 +0300

    Create post controller

--- server/Models/postModel.js
+++ server/Models/postModel.js
@@ -11,3 +11,6 @@ const postSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+var PostModel = mongoose.model("Posts", postSchema);
+export default PostModel;
--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { createPost, deletePost, getPost, likePost, updatePost } from "../Controllers/PostController.js";
+const router = express.Router()
 
-const router = express.Router();
-
-router.get("/", async (req, res) => {
-  res.send("Post Route");
-});
-
+router.post('/', createPost)
+router.get('/:id', getPost)
+router.put('/:id', updatePost)
+router.delete("/:id", deletePost)
+router.put("/:id/like", likePost)
 export default router;
