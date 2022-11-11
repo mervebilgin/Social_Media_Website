78c2982b5722895a45b906a915dd37b328d61f61
bilginmrv <mrvblgn44@gmail.com>
2022-11-11 20:59:51 +0300

    Create post route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import AuthRoute from "./Routes/AuthRoute.js";
 import UserRoute from "./Routes/UserRoute.js";
+import PostRoute from "./Routes/PostRoute.js";
 
 // Routes
 const app = express();
@@ -28,3 +29,4 @@ mongoose
 
 app.use("/auth", AuthRoute);
 app.use("/user", UserRoute);
+app.use("/post", PostRoute);
