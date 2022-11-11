d8b67ce8a1ed736179810b829d66993961bd9346
bilginmrv <mrvblgn44@gmail.com>
2022-11-11 22:11:38 +0300

    Timeline Post aggregation

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -1,5 +1,6 @@
 import PostModel from "../Models/postModel.js";
 import mongoose from "mongoose";
+import UserModel from "../Models/userModel.js";
 
 // Creat new Post
 export const createPost = async (req, res) => {
@@ -76,6 +77,46 @@ export const likePost = async (req, res) => {
       await post.updateOne({ $pull: { likes: userId } });
       res.status(200).json("Post Unliked");
     }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Get Timeline POsts
+export const getTimelinePosts = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const currentUserPosts = await PostModel.find({ userId: userId });
+    const followingPosts = await UserModel.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "following",
+          foreignField: "userId",
+          as: "followingPosts",
+        },
+      },
+      {
+        $project: {
+          followingPosts: 1,
+          _id: 0,
+        },
+      },
+    ]);
+
+    res.status(200).json(
+      currentUserPosts
+        .concat(...followingPosts[0].followingPosts)
+        .sort((a, b) => {
+          return b.createdAt - a.createdAt;
+        })
+    );
   } catch (error) {
     res.status(500).json(error);
   }
--- server/Routes/PostRoute.js
+++ server/Routes/PostRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost, deletePost, getPost, likePost, updatePost } from "../Controllers/PostController.js";
+import { createPost, deletePost, getPost, getTimelinePosts, likePost, updatePost } from "../Controllers/PostController.js";
 const router = express.Router()
 
 router.post('/', createPost)
@@ -7,4 +7,5 @@ router.get('/:id', getPost)
 router.put('/:id', updatePost)
 router.delete("/:id", deletePost)
 router.put("/:id/like", likePost)
+router.get("/:id/timeline", getTimelinePosts)
 export default router;
