3ded99ff434b9182dad3df75277f51857b2b0777
bilginmrv <mrvblgn44@gmail.com>
2022-11-13 01:26:56 +0300

    Update AuthReducer.js

--- client/src/reducers/AuthReducer.js
+++ client/src/reducers/AuthReducer.js
@@ -19,59 +19,6 @@ const authReducer = (
     case "AUTH_FAIL":
       return { ...state, loading: false, error: true };
 
-    // case "UPDATING_START":
-      return { ...state, updateLoading: true, error: false };
-
-    // case "UPDATING_SUCCESS":
-    //   localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
-    //   return {
-    //     ...state,
-    //     authData: action.data,
-    //     updateLoading: false,
-    //     error: false,
-    //   };
-
-    // case "UPDATING_FAIL":
-    //   return { ...state, updateLoading: true, error: true };
-
-    // case "LOG_OUT":
-    //   localStorage.clear();
-    //   return {
-    //     ...state,
-    //     authData: null,
-    //     loading: false,
-    //     error: false,
-    //     updateLoading: false,
-    //   };
-
-    // case "FOLLOW_USER":
-    //   return {
-    //     ...state,
-    //     authData: {
-    //       ...state.authData,
-    //       user: {
-    //         ...state.authData.user,
-    //         following: [...state.authData.user.following, action.data],
-    //       },
-    //     },
-    //   };
-
-    // case "UNFOLLOW_USER":
-    //   return {
-    //     ...state,
-    //     authData: {
-    //       ...state.authData,
-    //       user: {
-    //         ...state.authData.user,
-    //         following: [
-    //           ...state.authData.user.following.filter(
-    //             (personId) => personId !== action.data
-    //           ),
-    //         ],
-    //       },
-    //     },
-    //   };
-
     default:
       return state;
   }
