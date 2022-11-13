858ab1906ebcbe1b7cc139ee75ac1e77add069d5
bilginmrv <mrvblgn44@gmail.com>
2022-11-13 21:06:15 +0300

    Update files

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
-import "./App.css"
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import Auth from "./pages/Auth/Auth";
-// import Home from "./pages/home/Home";
 import Profile from "./pages/Profile/Profile";
+import { useSelector } from "react-redux"
 
 function App() {
+
+  const user = useSelector((state) => state.authReducer.authData);
   return (
     <div className="App">
-        <div className="blur" style={{top: '-18%', right: '0'}}></div>
-        <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+
+      <Routes>
+
+        <Route path='/' element={user?<Navigate to = "home"/>:<Navigate to = 'auth'/>} />
+
+        <Route path='/home' element={user?<Navigate to = '../auth'/>:<Home/>} />
+
+        <Route path='/auth' element={user?<Navigate to = '../home'/>:<Auth/>} />
         
-        {/* <Home/> */}
-        {/* <Profile/> */}
-        <Auth/>
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/ReduxStore";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
   </Provider>,
   
   document.getElementById("root")
