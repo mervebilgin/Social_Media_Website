c7038d6a9942714db68f98125744573d3ae37992
bilginmrv <mrvblgn44@gmail.com>
2022-11-30 21:56:01 +0300

    Like a  post

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -5,24 +5,35 @@ import Share from '../../img/share.png'
 import Heart from '../../img/like.png'
 import NotLike from '../../img/notlike.png'
 import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import { likePost } from '../../api/PostsRequests'
 
 
 const Post = ({data}) => {
   const {user} = useSelector((state)=>state.authReducer.authData)
 
+  const [liked, setLiked] = useState(data.likes.includes(user._id))
+  
+  const [likes, setLikes] = useState(data.likes.length)
+
+  const handleLike = () => {
+    setLiked((prev)=>!prev) 
+    likePost(data._id, user._id)
+    liked ? setLikes((prev) => prev -1) : setLikes((prev)=> prev + 1)
+  }
+
   return (
     <div className="Post">
         <img src={data.image? process.env.REACT_APP_PUBLIC_FOLDER + data.image: ""} alt="" />
 
-
         <div className="postReact">
-            <img src={data.liked?Heart: NotLike} alt="" />
+            <img src={liked?Heart: NotLike} alt="" style={{cursor: "pointer"}} onClick={handleLike} />
             <img src={Comment} alt="" />
             <img src={Share} alt="" />
         </div>
 
 
-        <span style={{color: "var(--gray)", fontSize: '12px'}}>{data.likes} likes</span>
+        <span style={{color: "var(--gray)", fontSize: '12px'}}>{likes} likes</span>
 
         <div className="detail">
             <span><b>{data.name}</b></span>
