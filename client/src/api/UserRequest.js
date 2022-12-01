1e600a934ea759be9c5fa69dc60647e763fb48b3
bilginmrv <mrvblgn44@gmail.com>
2022-12-01 22:13:11 +0300

    JWT Middleware

--- client/src/api/UserRequest.js
+++ client/src/api/UserRequest.js
@@ -6,7 +6,6 @@ API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
       req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
     }
-  
     return req;
   });
 
--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { deleteUser, followUser, getUser, UnFollowUser, updateUser, getAllUsers } from "../Controllers/UserController.js";
+import authMiddleWare from '../Middleware/AuthMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', getAllUsers)
 router.get('/:id', getUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
-router.put('/:id/follow', followUser)
-router.put('/:id/unfollow', UnFollowUser)
+router.put('/:id', authMiddleWare, updateUser)
+router.delete('/:id', authMiddleWare, deleteUser)
+router.put('/:id/follow', authMiddleWare, followUser)
+router.put('/:id/unfollow', authMiddleWare, UnFollowUser)
 export default router;
